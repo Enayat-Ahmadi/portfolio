21979f18d2abca532c69819497128efbb3e8e055
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-22 23:36:26 +0100

    add navbar desktop and mobile version

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import About from "./about/page";
 import ThemeToggle from "@/Components/ThemeToggle";
 import StarsBackground from "@/Components/StarBackground";
+import Navbar from "@/Components/Navbar";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
       <ThemeToggle />
       <StarsBackground />
+      <Navbar/>
     </div>
   );
 }
