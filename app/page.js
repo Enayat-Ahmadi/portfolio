b0064b4066c1d67d4edf4eb1d0ba6e5ecb2bf670
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-22 19:48:46 +0100

    remove default style from home page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import About from "./about/page";
+import ThemeToggle from "@/Components/ThemeToggle";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <About />
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>this is h1 in main</h1>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
+      <ThemeToggle/>
+      <main>
+        <h1>Hello from Home page</h1>
       </main>
     </div>
   );
